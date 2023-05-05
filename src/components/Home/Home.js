67a02e514732d8668f5844e8c47ac82f4e0ef609
ridhika koirala <ridhikakoirala@gmail.com>
2023-05-05 20:31:29 +0545

    forget password initialize

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,19 +3,12 @@ import SignIn from "./Signin";
 import Color from "../../Theme/Color"
 import HomeChess from "./HomeChess";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? Color.Primary : Color.Secondary,
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
 
 export default function Home() {
   return (
     <div>
       <Grid container spacing={2}>
-      <Grid item lg={6} md={6} sm={12} xs={12}>
+        <Grid item lg={6} md={6} sm={12} xs={12}>
           <SignIn />
         </Grid>
         <Grid item lg={6} md={6} sm={12} xs={12}>
